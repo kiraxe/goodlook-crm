bffba658e08c0dd5d732f8afe1ca53139edba283
kiraxe <kiraxe@yandex.ru>
2019-09-12 10:24:41 +0300

    goodlook-crm first commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,3 +6,29 @@ require('bootstrap/dist/js/bootstrap.min');
 require('popper.js/dist/popper.min');
 require('../../web/public/js/main.js');
 require('../../web/public/js/ordersForm.js');
+
+import { Calendar } from '@fullcalendar/core';
+import interactionPlugin from '@fullcalendar/interaction';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import listPlugin from '@fullcalendar/list';
+
+document.addEventListener('DOMContentLoaded', function() {
+    var calendarEl = document.getElementById('calendar');
+
+    var calendar = new Calendar(calendarEl, {
+        plugins: [ dayGridPlugin, timeGridPlugin, listPlugin, interactionPlugin ],
+        selectable: true,
+        header: {
+            left: 'prev,next today',
+            center: 'title',
+            right: 'dayGridMonth,timeGridWeek,timeGridDay'
+        },
+        dateClick: function (info) {
+            alert('clicked ' + info.dateStr);
+        },
+        select: function (info) {
+            alert('selected ' + info.startStr + ' to ' + info.endStr);
+        }
+    });
+});
