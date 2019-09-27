fa2af45d6e83ae5a2af0265e82f5f41d8240996d
kiraxe <kiraxe@yandex.ru>
2019-09-27 13:02:27 +0300

    goodlook-crm first commit

--- src/kiraxe/AdminCrmBundle/Controller/MaterialsController.php
+++ src/kiraxe/AdminCrmBundle/Controller/MaterialsController.php
@@ -62,7 +62,7 @@ class MaterialsController extends Controller
                     foreach ($workerorders as $workerorder) {
                         $residue += $workerorder->getAmountOfMaterial() + $workerorder->getMarriage();
                     }
-                    //$materials[$i]->setResidue($materials[$i]->getTotalsize() - $residue);
+                    $materials[$i]->setResidue($materials[$i]->getTotalsize() - $residue);
                     $em->persist($materials[$i]);
                     $em->flush();
                 }
