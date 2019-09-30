25af2817a864481d6db97155bd7105f7feddf79c
kiraxe <kiraxe@yandex.ru>
2019-09-30 11:14:10 +0300

    goodlook-crm first commit

--- web/public/js/cldr.js
+++ web/public/js/cldr.js
@@ -12,15 +12,15 @@ document.addEventListener('DOMContentLoaded', function() {
         d.setTime(ts);
         return d.getFullYear() + '-' + ('0' + (d.getMonth() + 1)).slice(-2) + '-' + ('0' + d.getDate()).slice(-2) + " " + ('0' + d.getHours()).slice(-2) + ":" + ('0' + d.getMinutes()).slice(-2);
     }
-    //var result;
+    var result;
 
     if (typeof notes != "undefined") {
 
-        //try {
+        try {
             var result = JSON.parse(notes);
-        //} catch(e) {
-            //result = notes;
-        //}
+        } catch(e) {
+            result = notes;
+        }
     }
 
     var calendar = new Calendar(calendarEl, {
