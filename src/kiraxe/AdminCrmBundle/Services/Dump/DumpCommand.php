8acfc07d3b79bab5a23d3dcfc232e25ce701efd7
kiraxe <kiraxe@yandex.ru>
2020-02-05 12:26:42 +0300

    goodlook-crm first commit

--- src/kiraxe/AdminCrmBundle/Services/Dump/DumpCommand.php
+++ src/kiraxe/AdminCrmBundle/Services/Dump/DumpCommand.php
@@ -31,7 +31,7 @@ class DumpCommand extends Command
     {
         $basePath = $this->progect_dir;
 
-        $message = shell_exec('bash '.$basePath.'\dump.sh');
+        $message = shell_exec('bash '.$basePath.'/dump.sh');
 
         $output->writeln($message);
     }
