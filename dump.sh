a037637408f4cfb4b5b362bb35546b8aab37eb56
kiraxe <kiraxe@yandex.ru>
2020-02-06 16:15:14 +0300

    goodlook-crm first commit

--- dump.sh
+++ dump.sh
@@ -1,9 +1,2 @@
 #!/bin/bash
 
-# See http://stackoverflow.com/questions/59895/can-a-bash-script-tell-what-directory-its-stored-in/23905052#23905052
-ROOT=$(readlink -f $(dirname "$0"))
-
-cd $ROOT
-
-
-
