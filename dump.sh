45b25173b01902cd6b41323eff452d70a9c51e7d
kiraxe <kiraxe@yandex.ru>
2020-02-06 16:08:58 +0300

    goodlook-crm first commit

--- dump.sh
+++ dump.sh
@@ -1,18 +1,5 @@
 #!/bin/bash
 
 # See http://stackoverflow.com/questions/59895/can-a-bash-script-tell-what-directory-its-stored-in/23905052#23905052
-ROOT=$(readlink -f $(dirname "$0"))
-
-cd $ROOT
-
-# Get database parameters
-dbname=$(grep "database_name" ./app/config/parameters.yml | cut -d " " -f 6)
-dbuser=$(grep "database_user" ./app/config/parameters.yml | cut -d " " -f 6)
-dbpassword=$(grep "database_password" ./app/config/parameters.yml | cut -d " " -f 6)
-filename="$(date '+%Y-%m-%d_%H-%M-%S').sql"
-
-
-cd $ROOT/web/public/crontab
-
 
 
